29aa45eeea6500fa19962f2b665aaac9b1d05ac8
SamraAzizi <samarraazizi@gmail.com>
2025-06-13 17:02:24 +0500

    periodic table

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/index.css';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
